851339863e0efdbfe88b8361503c2278d4ddf44a
Robert Flaugher <rlfprog@gmail.com>
2018-02-25 15:34:09 -0800

    First commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# Django REST Framework
+
+These are examples from [Django REST framework 2](http://www.tomchristie.com/rest-framework-2-docs/)
---
+++ snippets/views.py
@@ -0,0 +1,61 @@
+from django.http import HttpResponse
+from django.views.decorators.csrf import csrf_exempt
+from rest_framework.renderers import JSONRenderer
+from rest_framework.parsers import JSONParser
+from snippets.models import Snippet
+from snippets.serializers import SnippetSerializer
+
+class JSONResponse(HttpResponse):
+    """An HttpResponse that renders its content into JSON."""
+    def __init__(self, data, **kwargs):
+        content = JSONRenderer().render(data)
+        kwargs['content_type'] = 'application/json'
+        super(JSONResponse, self).__init__(content, **kwargs)
+
+@csrf_exempt
+def snippet_list(request):
+    """
+    List all code snippets, or create a new snippet.
+    """
+    if request.method == 'GET':
+        snippets = Snippet.objects.all()
+        # See http://www.tomchristie.com/rest-framework-2-docs/api-guide/serializers
+        # Serialize the queryset so that it can be rendered into JSON
+        # serializer.data is Python list
+        serializer = SnippetSerializer(snippets, many=True)
+        # Return data in JSON format
+        return JSONResponse(serializer.data)
+
+    elif request.method == 'POST':
+        data = JSONParser().parse(request)
+        serializer = SnippetSerializer(data=data)
+        if serializer.is_valid():
+            serializer.save()
+            return JSONResponse(serializer.data, status=201)
+        return JSONResponse(serializer.errors, status=400)
+
+@csrf_exempt
+def snippet_detail(request, pk):
+    """
+    Retrieve, update or delete a code snippet.
+    """
+    try:
+        snippet = Snippet.objects.get(pk=pk)
+    except Snippet.DoesNotExist:
+        return HttpResponse(status=404)
+
+    if request.method == 'GET':  # Retrieve
+        serializer = SnippetSerializer(snippet)
+        return JSONResponse(serializer.data)
+
+    elif request.method == 'PUT':  # Update
+        data = JSONParser().parse(request)
+        serializer = SnippetSerializer(snippet, data=data)
+        if serializer.is_valid():
+            serializer.save()
+            return JSONResponse(serializer.data)
+        return JSONResponse(serializer.errors, status=400)
+
+    elif request.method == 'DELETE':  # Delete
+        snippet.delete()
+        return HttpResponse(status=204)
